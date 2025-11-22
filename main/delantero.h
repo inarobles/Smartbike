250aee8ccc3c0c10a5f61fd8e8cef47c6c3595a7
inarobles <inarobles@gmail.com>
2025-11-22 21:08:15 +0100

    Subir todos los cambios del proyecto

--- main/delantero.h
+++
@@ -1,8 +0,0 @@
-#ifndef DELANTERO_H
-#define DELANTERO_H
-
-#include "lvgl.h"
-
-extern const lv_img_dsc_t delantero;
-
-#endif // DELANTERO_H
--- main/trasero.h
+++
@@ -1,8 +0,0 @@
-#ifndef TRASERO_H
-#define TRASERO_H
-
-#include "lvgl.h"
-
-extern const lv_img_dsc_t trasero;
-
-#endif // TRASERO_H
